668d95b2a77968b1214ac9e0335f6b85fef84894
25LP1024 25LP1024 <25LP1024@G301-CC0195.local>
2025-10-02 13:36:46 +0800

    10.2

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,35 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { nanoid } from "nanoid";
+import fs from "node:fs";
 
 const app: Application = express();
 const port = 8000;
 
-let tasks = [
-  { id: "1", name: "task 1", isChecked: false },
-  { id: "2", name: "task 2", isChecked: false },
-];
-
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-  console.log("Hello World");
-  res.send("Hello World11");
-});
+function loadTasksFromFile() {
+  const data = fs.readFileSync("data.txt", "utf8");
+  const tasks = JSON.parse(data);
+  return tasks;
+}
+
+function saveTasksToFile(
+  tasks: { id: string; name: string; isChecked: boolean }[]
+) {
+  fs.writeFile("data.txt", JSON.stringify(tasks), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("File written successfully");
+    }
+  });
+}
 
 app.get("/tasks", (req: Request, res: Response) => {
   // fetch database products
+  const tasks = loadTasksFromFile();
   res.send(tasks);
 });
 
@@ -33,15 +43,20 @@ app.post("/tasks", (req: Request, res: Response) => {
     return;
   }
 
+  const tasks = loadTasksFromFile();
+
   tasks.unshift({ id, name, isChecked: false });
+  saveTasksToFile(tasks);
+
   res.status(201).send([{ id }]); // Created
 });
 
 app.delete("/tasks/:id", (req: Request, res: Response) => {
   // delete a product from database
   const id = req.params.id;
-  const newTasks = tasks.filter((task) => task.id !== id);
-  tasks = newTasks;
+  const tasks = loadTasksFromFile();
+  const newTasks = tasks.filter((task: { id: string }) => task.id !== id);
+  saveTasksToFile(newTasks);
   res.sendStatus(204); // No Content
 });
 
@@ -50,12 +65,14 @@ app.put("/tasks/:id", (req, res) => {
   const id = req.params.id;
   const name = req.body.name;
   const isChecked = req.body.isChecked;
-  const taskIndex = tasks.findIndex((task) => task.id === id);
+  const tasks = loadTasksFromFile();
+  const taskIndex = tasks.findIndex((task: { id: string }) => task.id === id);
   tasks[taskIndex].name = name;
   tasks[taskIndex].isChecked = isChecked;
+  saveTasksToFile(tasks);
   res.sendStatus(204); // No Content
 });
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-// xaxa
